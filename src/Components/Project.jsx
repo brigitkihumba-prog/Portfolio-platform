9135bfeee1928435ef0eda82e3f4d34ea9c61740
Brigit Kihumba <brigitkihumba@gmail.com>
2026-08-31 23:06:16 +0300

    changes

---
+++ src/Components/Project.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Project() {
+  return (
+    <>
+    <div>
+        <h1>Project List</h1>
+    </div>
+
+    </>
+    
+  )
+}
+
+export default Project
---
+++ src/Components/ProjectList.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Project from './Project'
+
+function PortfolioList() {
+  return (
+    <>
+        <div>
+            <h1>Portfolio List</h1>
+            <Project /> 
+        </div>      
+    </>
+    
+  )
+}
+
+export default PortfolioList
